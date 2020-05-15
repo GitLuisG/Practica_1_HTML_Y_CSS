24e4c766fb564b60778217867c4573ca92e6b8e2
Luis Gerardo Perales Torres <1730505@upv.edu.mx>
2020-05-14 19:45:20 -0500

    Actualizacion js

--- Practica_1_HTMLYCSS/js/index.js
+++ Practica_1_HTMLYCSS/js/index.js
@@ -7,6 +7,9 @@ function btnEnviar_click(e){
     alert('hola '+txtNombre.value+' Sus datos se estan enviando...');
 }
 
+btnEnviar.addEventListener('keyup',(e)=>{
+    console.log('txtNombre: '+ txtNombre.value);
+});
 
 const btnEnviar1_click = (e)=>{
     alert('hola mundo!');
@@ -14,3 +17,26 @@ const btnEnviar1_click = (e)=>{
 
 btnEnviar.addEventListener('click',btnEnviar1_click);
 
+function btnEnviar_click(e){
+    if(!txtNombre.value){
+        alert('No ha proporcionado el nombre.');
+        txtNombre.focus();
+        return;
+    }
+    alert('Hola ' + txtNombre.value + ' sus datos se estan enviando...');
+}
+
+let spanA = document.getElementById('spanA');
+spanA.addEventListener('click', spanAB_click);
+let spanB = document.getElementById('spanB');
+spanB.addEventListener('click', spanAB_click);
+let tabla = document.getElementById('tabla-datos');
+
+function spanAB_click(e){
+    let spanId = this.id;
+    if(spanId == 'spanA'){
+        tabla.style.display = 'none';
+    }else{
+        tabla.style.display = 'block';
+    }
+}
