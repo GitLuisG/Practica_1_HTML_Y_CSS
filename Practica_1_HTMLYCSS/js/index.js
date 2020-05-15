07d5e343f6d770a5eefcfedd18c050aded76778b
Luis Gerardo Perales Torres <1730505@upv.edu.mx>
2020-05-14 19:17:05 -0500

    js

---
+++ Practica_1_HTMLYCSS/js/index.js
@@ -0,0 +1,16 @@
+let btnEnviar = document.getElementById('btnEnviar');
+btnEnviar.addEventListener('click',btnEnviar_click);
+
+let txtNombre = document.getElementById('txtNombre');
+
+function btnEnviar_click(e){
+    alert('hola '+txtNombre.value+' Sus datos se estan enviando...');
+}
+
+
+const btnEnviar1_click = (e)=>{
+    alert('hola mundo!');
+}
+
+btnEnviar.addEventListener('click',btnEnviar1_click);
+
